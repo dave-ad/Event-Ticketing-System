473082eab7aaad87e4102ecc7c0a2972172fbe04
dave-ad <davidaderibigbe47@gmail.com>
2024-04-04 01:29:41 +0100

    Add project files.

---
+++ LinearSearch/Program.cs
@@ -0,0 +1,115 @@
+﻿using LinearSearch;
+
+namespace Algorithms;
+
+class Program
+{
+    /// <summary>
+    /// Use to seach values on int arrays
+    /// </summary>
+    /// <param name="collection">The colllection that the search is going to be executed on</param>
+    /// <param name="itemToSearch">The integer value that we search in the collection</param>
+    /// <returns>The index of the search item. Returns -1 if not found</returns>
+    static int Search(int[] collection, int itemToSearch)
+    {
+        for (int i = 0; i < collection.Length; i++)
+        {
+            if (collection[i] == itemToSearch)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static void Main(string[] args)
+    {
+        // Searching Algorithm - Linear search
+        // Returns the first element that applies for the condition
+        // The longer the collection the more step it needs to succeed
+        // Works even if the collections is not oredered
+
+        int[] collection = { 0, 1, 2, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine("The collections: ");
+        for (int i = 0; i < collection.Length; i++)
+            Console.Write(collection[i] + ", ");
+
+        Console.WriteLine();
+
+        //int objectOfSearch = int.Parse(Console.ReadLine());
+        int objectOfSearch = 2;
+        Console.WriteLine($"Searching item {objectOfSearch} in the collection...");
+        int searchResult = Search(collection, objectOfSearch);
+
+        if (searchResult != -1)
+            Console.WriteLine($"Value found at index {searchResult}");
+        else
+            Console.WriteLine("Value not found in the collection");
+
+        Console.WriteLine("\n--------------------------------------------------------------------------------------------------\n");
+        Console.ReadLine();
+
+        Person[] persons =
+        {
+            new Person
+            {
+                Name = "Adam",
+                Age = 31,
+            },
+
+            new Person
+            {
+                Name = "Melissa",
+                Age = 30,
+            },
+
+            new Person
+            {
+                Name = "Perselus",
+                Age = 45,
+            },
+
+            new Person
+            {
+                Name = "Clark",
+                Age = 31,
+            },
+
+            new Person
+            {
+                Name = "Jane",
+                Age = 21,
+            }
+        };
+
+        persons.ToList().ForEach(x => { Console.WriteLine(x); });
+
+        //string nameToFind = Console.ReadLine();
+        string nameToFind = "Adam";
+        Person resultOfTheSearch = FindPersonByName(persons, nameToFind);
+
+        Console.WriteLine($"Searching for {nameToFind} in the database...");
+
+        if (resultOfTheSearch !=null)
+            Console.WriteLine(resultOfTheSearch.ToString());
+        else
+            Console.WriteLine("Sorry, this guy is not present here");
+
+        Console.ReadLine();
+
+    }
+
+    // Working with complex data
+    static Person FindPersonByName(Person[] persons, string nameToSearch)
+    {
+        foreach (var person in persons)
+        {
+            if (person.Name == nameToSearch)
+            {
+                return person;
+            }
+        }
+        return null;
+    }
+}
---
+++ MakingDecisions/Program.cs
@@ -0,0 +1,63 @@
+﻿class Program
+{
+
+    static bool HasValueAboveTen(int[] collection)
+    {
+        for (int i = 0; i < collection.Length; i++)
+        {
+            if (collection[i] > 10)
+                return true;
+        }
+        return false;
+    }
+
+    static bool HasValueAbove(int[] collection, int value = 0)
+    {
+        for (int i = 0; i < collection.Length; i++)
+        {
+            if (collection[i] > value)
+                return true;
+        }
+
+        return false;
+    }
+
+    static bool Decide(int[] collection, Func<int, bool> condition)
+    {
+        for (int i = 0; i < collection.Length; i++)
+        {
+            if (condition(collection[i]))
+                return true;
+        }
+        return false;
+    }
+
+    static bool IsNegative(int number)
+    {
+        if (number < 0)
+            return true;
+        else
+            return false;
+    }
+
+    static void Main(string[] args)
+    {
+        // Making Decisions 
+        // Checking if our collection has a certian type of element
+        // We need: collection, condition
+
+        int[] arrays = { 0, 1, 2, 3, 4, 5, 6, 7, 8, -9, 10};
+
+        Console.WriteLine($"The collection has an element that is higher than ten : {HasValueAboveTen(arrays)}");
+
+        //int index = 3;
+        //bool hasValueOverThree = HasValueAbove(arrays, index);
+        //Console.WriteLine($"The collection has value above {index} ; {hasValueOverThree}");
+        Console.WriteLine($"The only number above 0 ; {HasValueAbove(arrays)}");
+
+        bool hasNegative = Decide(arrays, IsNegative);
+        Console.WriteLine($"The collection has a negative number {hasNegative}");
+
+        Console.ReadLine();
+    }
+}
